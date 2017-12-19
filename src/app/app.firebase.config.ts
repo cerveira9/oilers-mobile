a71f38054472c777fb2849aafeeff7dc063f9fb0
Fernando Cerveira <fernandocerveira@Fernandos-MacBook-Pro.local>
2017-12-19 14:30:22 -0200

    Firebase config

---
+++ src/app/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyAl13kD_IulQ_p_WfUtgxkc2cDXcVzSHgY",
+    authDomain: "oilersauth.firebaseapp.com",
+    databaseURL: "https://oilersauth.firebaseio.com",
+    projectId: "oilersauth",
+    storageBucket: "",
+    messagingSenderId: "809899382385"
+  };
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,48 @@
+import * as firebase from 'firebase';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPage } from '../pages/login/login';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAl13kD_IulQ_p_WfUtgxkc2cDXcVzSHgY",
+  authDomain: "oilersauth.firebaseapp.com",
+  databaseURL: "https://oilersauth.firebaseio.com",
+  projectId: "oilersauth",
+  storageBucket: "",
+  messagingSenderId: "809899382385"
+};
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    LoginPage
   ],
   providers: [
     StatusBar,
